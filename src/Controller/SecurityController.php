306da0e1cb03a90e8a1888a3ba5aebc23995ed18
Moetaz-doghman <61354236+Moetaz-doghman@users.noreply.github.com>
2021-07-15 14:41:41 +0100

    loginuser

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,11 +25,34 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+    /**
+     * @Route("/loginuser", name="app_user")
+     */
+    public function loginuser(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/user.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
 
     /**
      * @Route("/logout", name="app_logout")
      */
     public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+     /**
+     * @Route("/logoutuser", name="app_logoutuser")
+     */
+    public function logoutuser()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
